7651b187e81e61ef678aa4ef02f51b5f4166d178
aruizgg <antoniorzgar@gmail.com>
2025-02-14 20:33:37 +0100

    Implementación de la recepción de parámetros de entrada

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -14,16 +15,41 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Definir los comandos disponibles
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+	completeCmd := flag.NewFlagSet("complete", flag.ExitOnError)
+	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+
 	// Selección de comando
 	switch os.Args[1] {
 	case "add":
-		fmt.Println("Comando: Agregar tarea")
+		addTask := addCmd.String("task", "", "Nombre de la tarea a agregar")
+		addCmd.Parse(os.Args[2:])
+		if *addTask == "" {
+			fmt.Println("Debes especificar una tarea con -task")
+			os.Exit(1)
+		}
+		fmt.Println("Tarea agregada:", *addTask)
 	case "list":
-		fmt.Println("Comando: Listar tareas")
+		listCmd.Parse(os.Args[2:])
+		fmt.Println("Mostrando todas las tareas...")
 	case "complete":
-		fmt.Println("Comando: Completar tarea")
+		completeTaskID := completeCmd.Int("id", -1, "ID de la tarea a marcar como completada")
+		completeCmd.Parse(os.Args[2:])
+		if *completeTaskID == -1 {
+			fmt.Println("Debes especificar un ID de tarea con -id")
+			os.Exit(1)
+		}
+		fmt.Println("Tarea completada con ID:", *completeTaskID)
 	case "delete":
-		fmt.Println("Comando: Eliminar tarea")
+		deleteTaskID := deleteCmd.Int("id", -1, "ID de la tarea a eliminar")
+		deleteCmd.Parse(os.Args[2:])
+		if *deleteTaskID == -1 {
+			fmt.Println("Debes especificar un ID de tarea con -id")
+			os.Exit(1)
+		}
+		fmt.Println("Tarea eliminada con ID:", *deleteTaskID)
 	default:
 		fmt.Println("Comando no reconocido:", os.Args[1])
 		fmt.Println("Comandos disponibles: add, list, complete, delete")
